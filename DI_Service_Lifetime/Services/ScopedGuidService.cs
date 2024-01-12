87a310280c772860189f64eb0aacdcb446f6414c
ROHAN8774 <rohan10bhakte0708@gmail.com>
2024-01-12 13:07:25 +0530

    Add project files.

---
+++ DI_Service_Lifetime/Services/ScopedGuidService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DI_Service_Lifetime.Services
+{
+	public class ScopedGuidService : IScopedGuidService
+	{
+		private readonly Guid Id;
+		public ScopedGuidService()
+		{
+			Id = Guid.NewGuid();
+		}
+		public string GetGuid()
+		{
+			return Id.ToString();
+		}
+	}
+}
